7e6b124b8756a9b393d90694f607fae8f57b8619
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-08 11:50:59 -0600

    fixed again?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    # origins 'localhost:3000'
+    origins 'https://todo-listsb-server.herokuapp.com/api/v1/'
     # origins ["localhost:3000", ""]
     resource '*',
       headers: :any,
