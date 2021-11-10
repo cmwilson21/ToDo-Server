6f79035a9aae3581f6afa55698a48b190d92194e
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-10 11:21:34 -0600

    added pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,7 @@ class ApplicationController < ActionController::API
 
   def current_user
     if decoded_token
+      binding.pry
       # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
       # or nil if we can't decode the token
       user_id = decoded_token[0]['user_id']
