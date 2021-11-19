3fc8e775e60e87d5c23318ca718cbd1600bc2c41
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-19 13:44:31 -0600

    added user model tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,6 +2,56 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  it "exist" do
+    expect {User}.to_not raise_error
+  end
+
+  it "has a first name, last name, dob, email, and password" do
+    user = User.new(first_name: "Hank", last_name: "Hill", email: "hank@propane.com", password: "password", date_of_birth: 01/01/1990)
+    expect(user.first_name).to eq("Hank")
+    expect(user.last_name).to eq("Hill")
+    expect(user.date_of_birth).to eq(01/01/1990)
+    expect(user.email).to eq("hank@propane.com")
+    expect(user.password).to eq("password")
+  end
+
+  context "validating a user" do
+    before(:each) do
+      @hank = User.new(first_name: "Hank", last_name: "Hill", email: "hank@propane.com", password: "password", date_of_birth: 01/01/1990)
+    end
+
+    it "requires a first name" do
+      @hank.first_name = nil
+      expect(@hank).to_not be_valid
+    end
+
+    it "requires a last name" do
+      @hank.last_name = nil
+      expect(@hank).to_not be_valid
+    end
+
+    it "requires a date of birth" do
+      @hank.date_of_birth = nil
+      expect(@hank).to_not be_valid
+    end
+
+    it "requires an email" do
+      @hank.email = nil
+      expect(@hank).to_not be_valid
+    end
+    
+    it "requires a password" do
+      @hank.password = nil
+      expect(@hank).to_not be_valid
+    end
+
+    # it "has a unique email" do
+    #   @bobby = User.new(first_name: "Bobby", last_name: "Hill", email: "hank@propane.com", date_of_birth: 01/01/2001)
+    #   expect(hank2).to_not be_valid
+  # end
+
+ 
+  end
 
 
 end
